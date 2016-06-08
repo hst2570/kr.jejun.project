9d1b2003b056931d60609acbac67082a5563b214
hst2570 <hst2570@gmail.com>
2016-06-09 00:42:50 +0900

    add app description

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -16,3 +16,19 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
+
+/*
+    * 기능
+    1. 회원가입
+     - ID, password, name, des, image
+     - 프로필은 수정 가능해야 한다.
+    2. 글
+     - 메인은 모든 글을 보여준다.
+     - 개인 저장기능
+     - 자신의 글은 삭제 가능
+    3. 추천, 반대
+     - 한 사람은 하나의 추천이나 반대 액션을 할 수 있다.
+
+    * 데이터베이스
+     - user, comment, action(?)
+ */
