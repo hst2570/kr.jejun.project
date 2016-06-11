8431f47cd50c284e0d64ec5f42ee46421ee190d9
hst2570 <hst2570@gmail.com>
2016-06-11 15:44:55 +0900

    add controller

--- src/main/java/main/controller/ActionController.java
+++ src/main/java/main/controller/ActionController.java
@@ -1,7 +1,16 @@
 package main.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by user on 2016-06-10.
  */
 public class ActionController {
+    /*
+        추천 반대
+     */
+    @RequestMapping("/action")
+    public String action(){
+        return "action";
+    }
 }
--- src/main/java/main/controller/CommentController.java
+++ src/main/java/main/controller/CommentController.java
@@ -1,10 +1,24 @@
 package main.controller;
 
+import main.domain.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by user on 2016-06-10.
  */
 public class CommentController {
     /*
-
+        1. 로그인창
+        2. 글 작성
      */
+    @RequestMapping("/comment")
+    public String comment(){
+        return "comment";
+    }
+
+    @RequestMapping("/comment/write")
+    public String commentWrite(){
+        return "commentWrite";
+    }
+
 }
